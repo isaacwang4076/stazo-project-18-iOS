c780e023c85fb22bc144d517b2fb30af9fb70dc8
Isaac Wang <isaacwang4076@gmail.com>
2016-07-12 15:53:24 -0700

    User and Event pull/push functionality in place

--- project-18/User.swift
+++ project-18/User.swift
@@ -12,17 +12,17 @@ class User {
     
     // Constants
     let fb = Globals.fb
-    let DEFAULT_BIO: String = "This user does not have a bio."
+    let DEFAULT_BIO = "This user does not have a bio."
     
     // Variables
     var userID: String?
     var userName: String?
     var bio: String?
-    var myEvents = [String]?()
-    var attendingEvents = [String]?()
-    var reportedEvents = [String]?()
-    var userTrails = [String]?()
-    var userFollowers = [String]?()
+    var myEvents: [String]? = []
+    var attendingEvents: [String]? = []
+    var reportedEvents: [String]? = []
+    var userTrails: [String]? = []
+    var userFollowers: [String]? = []
     var friends = [String: String]()
     
     
@@ -38,19 +38,23 @@ class User {
     // - Takes in a json object (snapshot) and constructs
     //   a user out of it.
     init(userDict: NSDictionary) {
-        self.attendingEvents = userDict.valueForKey("attendingEvents") as! [String]?
+        self.attendingEvents = (userDict.valueForKey("attendingEvents") as? NSDictionary)?.allValues as! [String]?
         self.bio = userDict.valueForKey("bio") as! String?
         self.userID = userDict.valueForKey("id") as! String?
-        self.myEvents = userDict.valueForKey("myEvents") as! [String]?
+        self.myEvents = (userDict.valueForKey("myEvents") as? NSDictionary)?.allValues as! [String]?
         self.userName = userDict.valueForKey("name") as! String?
-        self.userFollowers = userDict.valueForKey("userFollowers") as! [String]?
-        self.userTrails = userDict.valueForKey("userTrails") as! [String]?
+        self.userFollowers = userDict.valueForKey("userFollowers") as? [String]
+        self.userTrails = userDict.valueForKey("userTrails") as? [String]
     }
     
+    // PUSH TO FIREBASE
+    // - Add this user to firebase "Users" section
     func pushToFirebase() {
         fb.child("Users").child(userID!).setValue(convertToDictionary())
     }
     
+    // CONVERT TO DICTIONARY
+    // - Helper function for pushToFirebase(), converts user into dictionary
     func convertToDictionary() -> NSDictionary {
         return [
         "attendingEvents": attendingEvents!,
@@ -60,6 +64,14 @@ class User {
         "name": userName!,
         "userFollowers": userFollowers!,
         "userTrails": userTrails!]
+    }
+    
+    // TOSTRING METHOD
+    // - Just for checking that the user has the right info
+    func toString() {
+        print("\nUser toString()\n\nuserName is : ", userName, "\nattendingEvents is: ", attendingEvents, "\nbio is: ", bio,
+              "\nuserID is : ", userID, "\nmyEvents is: ",
+              myEvents, "\nuserFollowers is : ", userFollowers, "\nuserTrails is : ", userTrails)
     }
     
 }
